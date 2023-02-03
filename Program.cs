cd1eb49041dd2fe64e9f3457218bb28242b9159e
Mark Novak <mark@m8it.io>
2023-02-03 11:44:57 +0100

    Restructure and refactor of interaction logic
    Implements a class to execute interaction logic, simplifying the interaction between the file service and itself.

--- Program.cs
+++ Program.cs
@@ -1,99 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SvgPathExtractor;
 
-FileService fileService = new();
-ConsoleKey? response;
-Console.WriteLine("Svg path extractor");
-do
-{
-    Console.Write("Are .svg files located in currently residing folder? (Y/N)");
-    response = Console.ReadKey(false).Key;
-    if (response is ConsoleKey.N)
-    {
-        ReadFilesFromAnotherPath(fileService);
-    }
-    else if (response is ConsoleKey.Y)
-    {
-        ReadFilesFromCurrentPath(fileService);
-    }
-}
-while (response is not ConsoleKey.N && response is not ConsoleKey.Y);
+ConsoleLogic logic = new ConsoleLogic();
 
-response = null;
+await logic.Start();
 
-do
-{
-    Console.WriteLine("Do you want to save output anywhere else? (Y/N)");
-    response = Console.ReadKey(false).Key;
-    if (response is ConsoleKey.Y)
-    {
-        OutputFilesToAnotherPath();
-        await WriteFilesToAnotherPath(fileService);
 
-    }
-    else if (response is ConsoleKey.N)
-    {
-        await WriteFilesToLocalPath(fileService);
-    }
-}
-while (response is not ConsoleKey.N && response is not ConsoleKey.Y);
 
-Console.Write("\noperation done. Press any key to continue...");
-Console.ReadKey(true);
-
-void ReadFilesFromAnotherPath(FileService fileService)
-{
-    Console.Clear();
-    bool isValid = false;
-    do
-    {
-        Console.WriteLine("Please enter the folder file path:");
-        fileService.FolderPath = Console.ReadLine() ?? "";
-        if (string.IsNullOrWhiteSpace(fileService.FolderPath))
-        {
-            Console.WriteLine("Incorrect input. please try again!\n");
-        }
-        else
-            isValid = true;
-    }
-    while (!isValid);
-    fileService.ReadAllFiles();
-}
-
-void ReadFilesFromCurrentPath(FileService fileService)
-{
-    Console.Clear();
-    fileService.ReadAllFiles();
-}
-
-void OutputFilesToAnotherPath()
-{
-    bool isValid = false;
-    Console.Clear();
-    do
-    {
-        Console.WriteLine("Where would you like to output the result?");
-        Console.Write("Path: ");
-        fileService.OutputPath = Console.ReadLine() ?? "";
-        if (string.IsNullOrWhiteSpace(fileService.OutputPath))
-        {
-            Console.WriteLine("Incorrect input. please try again!\n");
-        }
-        else
-            isValid = true;
-    }
-    while (!isValid);
-}
-
-async Task WriteFilesToAnotherPath(FileService fileService)
-{
-    await fileService.WriteOutputToJson();
-}
-async Task WriteFilesToLocalPath(FileService fileService)
-{
-    fileService.CreateNewDirectory();
-    await fileService.WriteOutputToJson();
-}
 
 
 
